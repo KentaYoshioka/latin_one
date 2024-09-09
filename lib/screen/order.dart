345bceb305ba90c3b78404e8cbfb88f96aaeff55
SenoOh <pwmj9579@s.okayama-u.ac.jp>
2024-09-09 17:26:20 +0900

    Update displaying shop information and products received as return values

--- lib/screen/order.dart
+++ lib/screen/order.dart
@@ -7,11 +7,15 @@ class OrderPage extends StatefulWidget {
   @override
   State<OrderPage> createState() => _OrderPageState();
 }
-
 class _OrderPageState extends State<OrderPage> {
-  String shops = '';
+  String shops = '';  // 文字列用の変数
+  List<Map<String, dynamic>> products = [];
+  
   @override
   Widget build(BuildContext context) {
+    // double totalAmount = products.fold(0, (sum, product) {
+    //   return sum + (product['totalPrice'] as double);
+    // });
     return Scaffold(
       appBar: AppBar(
         title: Text('Order'),
@@ -23,50 +27,134 @@ class _OrderPageState extends State<OrderPage> {
             children: <Widget>[
               GestureDetector(
                 onTap: () async {
-                  String shopinfo = await Navigator.push(
-                      context, MaterialPageRoute(builder: (context){
-                        return OrderShopsPage();
-                        },
-                  ));
+                  final shopinfo = await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return OrderShopsPage();
+                    }),
+                  );
+                  print(shopinfo);
                   if (shopinfo != null) {
-                    await Navigator.push(
-                        context, MaterialPageRoute(builder: (context) {
-                          return ProductPage();
-                          },
-                    ));
+                    setState(() {
+                      shops = shopinfo;
+                    });
+                    final product = await Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return ProductPage();
+                      }),
+                    );
+                    print(product);
+                    if (product != null) {
+                      setState(() {
+                        products = product;
+                      });
+                      print(products);
+                    }
                   }
                 },
                 child: Container(
-                    margin: EdgeInsets.all(10), height:100 ,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Text(
-                        'select shops',
-                        style: Order_Style
-                    ),
-                    alignment: Alignment.center
+                  margin: EdgeInsets.all(10),
+                  height: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.store, size: 40, color: Colors.blueAccent),
+                      SizedBox(width: 10),
+                      Text(
+                        'Select Shops',
+                        style: Order_Style,
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
                 ),
               ),
               GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => ProductPage()));
-                },
+                onTap: shops.isNotEmpty ? () async {
+                  final product = await Navigator.push(
+                    context, MaterialPageRoute(builder: (context) {
+                      return ProductPage();
+                    }),
+                  );
+                  print(product);
+                  if (product != null) {
+                    setState(() {
+                      products = product;
+                    });
+                    print(products);
+                  }
+                }
+                : null,
                 child: Container(
-                    margin: EdgeInsets.all(10), height:100 ,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Text(
-                        'select product',
-                        style: Order_Style
-                    ),
-                    alignment: Alignment.center
+                  margin: EdgeInsets.all(10),
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: shops.isNotEmpty ? Colors.white : Colors.grey[300],
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.shopping_cart,
+                        size: 40,
+                        color: shops.isNotEmpty ? Colors.green : Colors.grey,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Select Product',
+                        style: Order_Style.copyWith(
+                          color: shops.isNotEmpty ? Colors.black : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
                 ),
               ),
+              shops.isNotEmpty
+                  ? Container(
+                      // margin: EdgeInsets.all(10),
+                      height: 100,
+                      // alignment: Alignment.center,
+                      child: Text(
+                        '$shops',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  : SizedBox.shrink(),
+              products.isNotEmpty ? Container(
+                margin: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                child: Column(
+                  children: products.map((product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,  // タイトルと価格を左右に分けて表示
+                      children: [
+                        Text(
+                          product['title'],  // 各productのtitleを表示
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          '${product['quantity']}g',  // 各productのtotalPriceを表示
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          '¥${product['totalPrice']}',  // 各productのtotalPriceを表示
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              )
+                  : SizedBox.shrink(),
             ],
           ),
         ),
