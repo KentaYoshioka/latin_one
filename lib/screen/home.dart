8d1639dab5b0002ea0b90176d70193d9e333f7f7
KentaYoshioka <ky19991008@gmail.com>
2024-10-09 17:49:55 +0900

    Update for network disconnection

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import './product.dart';
 import './topic2.dart';
 import '../page.dart';
 import '../style.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  Future<void> _checkConnectivity(BuildContext context) async {
+    final ConnectivityResult result = await Connectivity().checkConnectivity();
+    if (result == ConnectivityResult.none) {
+      _showNoConnectionDialog(context);
+    }
+  }
+
+  void _showNoConnectionDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('ネットワーク接続なし'),
+          content: const Text('インターネットに接続されていません。接続を確認してください。'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    _checkConnectivity(context);
+
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(32.0),
@@ -21,7 +51,7 @@ class HomePage extends StatelessWidget {
                   mainPageState?.onItemTapped(2);
                 },
                 child: Container(
-                    margin: const EdgeInsets.all(10), width: 350, height:200 ,
+                    margin: const EdgeInsets.all(10), width: 350, height: 200,
                     decoration: background_image('assets/images/coffee.jpg'),
                     alignment: Alignment.center,
                     child: const Text(
@@ -36,7 +66,7 @@ class HomePage extends StatelessWidget {
                       context, MaterialPageRoute(builder: (context) => const ProductPage(isFromHomePage: true)));
                 },
                 child: Container(
-                    margin: const EdgeInsets.all(10), width: 350, height:200 ,
+                    margin: const EdgeInsets.all(10), width: 350, height: 200,
                     decoration: background_image('assets/images/IMG_8836.jpg'),
                     alignment: Alignment.center,
                     child: const Text(
@@ -51,7 +81,7 @@ class HomePage extends StatelessWidget {
                       context, MaterialPageRoute(builder: (context) => const Topic2Page()));
                 },
                 child: Container(
-                    margin: const EdgeInsets.all(10), width: 350, height:200 ,
+                    margin: const EdgeInsets.all(10), width: 350, height: 200,
                     decoration: background_image('assets/images/IMG_8837.jpg'),
                     alignment: Alignment.center,
                     child: const Text(
