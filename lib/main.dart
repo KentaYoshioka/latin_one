795c902144796daa954ce8f472575461bce38ae0
KentaYoshioka <ky19991008@gmail.com>
2024-11-14 11:40:41 +0900

    Update for inbox and handling network-error

--- lib/main.dart
+++ lib/main.dart
@@ -2,107 +2,153 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'dart:convert';
-import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:convert'; // jsonを使用するため
+import 'package:flutter/foundation.dart'; // kIsWebを使用
+
 import './page.dart' as page;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
-  await Supabase.initialize(
-    url: dotenv.get('SUPABASE_URL'),
-    anonKey: dotenv.get('SUPABASE_KEY'),
-  );
-
-  await Firebase.initializeApp();
-
-  final messagingInstance = FirebaseMessaging.instance;
-  messagingInstance.requestPermission();
-
-  final fcmToken = await messagingInstance.getToken();
-  debugPrint('FCM TOKEN: $fcmToken');
-
-  final fcmtopic = await messagingInstance.subscribeToTopic('allUsers');
-
-  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  if (Platform.isAndroid) {
-    final androidImplementation =
-    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
-        AndroidFlutterLocalNotificationsPlugin>();
-    await androidImplementation?.createNotificationChannel(
-      const AndroidNotificationChannel(
-        'default_notification_channel',
-        'プッシュ通知のチャンネル名',
-        importance: Importance.max,
-      ),
-    );
-    await androidImplementation?.requestNotificationsPermission();
-  }
-
-  // 通知設定の初期化を行う
-  _initNotification();
-
-  // アプリ停止時に通知をタップした場合はgetInitialMessageでメッセージデータを取得できる
-  final message = await FirebaseMessaging.instance.getInitialMessage();
-  // 取得したmessageを利用した処理などを記載する
 
   runApp(const MaterialApp(
-          title: 'LatinOne',
-          home: page.Page(title: 'LatinOne'),
-      ));
+    title: 'LatinOne',
+    home: ConnectionChecker(),
+  ));
 }
 
-Future<void> _initNotification() async {
-  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+class ConnectionChecker extends StatefulWidget {
+  const ConnectionChecker({Key? key}) : super(key: key);
 
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    // バックグラウンド起動中に通知をタップした場合の処理
-  });
+  @override
+  _ConnectionCheckerState createState() => _ConnectionCheckerState();
+}
 
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-    final notification = message.notification;
-    debugPrint('Message: $notification');
-    final android = message.notification?.android;
+class _ConnectionCheckerState extends State<ConnectionChecker> {
+  bool _isDialogShowing = false;
 
-    // フォアグラウンド起動中に通知が来た場合の処理
+  @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+  }
 
-    // フォアグラウンド起動中に通知が来た場合、
-    // Androidは通知が表示されないため、ローカル通知として表示する
-    // https://firebase.flutter.dev/docs/messaging/notifications#application-in-foreground
-    if (Platform.isAndroid) {
-      // プッシュ通知をローカルから表示する
-      await FlutterLocalNotificationsPlugin().show(
-        0,
-        notification!.title,
-        notification.body,
-        NotificationDetails(
-          android: AndroidNotificationDetails(
-            'default_notification_channel',
-            'プッシュ通知のチャンネル名',
-            importance: Importance.max, // 通知の重要度の設定
-            icon: android?.smallIcon,
-          ),
+  Future<void> _checkConnectivity() async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      _showNoConnectionDialog();
+    } else {
+      await _initializeApp();
+    }
+  }
+
+  Future<void> _initializeApp() async {
+    await dotenv.load(fileName: '.env');
+    await Supabase.initialize(
+      url: dotenv.maybeGet('SUPABASE_URL') ?? 'default_url',
+      anonKey: dotenv.maybeGet('SUPABASE_KEY') ?? 'default_key',
+    );
+
+    try {
+      await Firebase.initializeApp();
+    } catch (e) {
+      debugPrint('Firebase initialization failed: $e');
+    }
+
+    final messagingInstance = FirebaseMessaging.instance;
+    messagingInstance.requestPermission();
+
+    try {
+      final fcmToken = await messagingInstance.getToken();
+      debugPrint('FCM TOKEN: $fcmToken');
+      await messagingInstance.subscribeToTopic('allUsers');
+    } catch (e) {
+      debugPrint('Failed to get FCM token or subscribe to topic: $e');
+    }
+
+    final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    if (!kIsWeb) {
+      final androidImplementation = flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>();
+      await androidImplementation?.createNotificationChannel(
+        const AndroidNotificationChannel(
+          'default_notification_channel',
+          'プッシュ通知のチャンネル名',
+          importance: Importance.max,
         ),
-        payload: json.encode(message.data),
       );
     }
-  });
 
-  // ローカルから表示したプッシュ通知をタップした場合の処理を設定
-  flutterLocalNotificationsPlugin.initialize(
-    const InitializationSettings(
-      android: AndroidInitializationSettings(
-          '@mipmap/ic_launcher'), //通知アイコンの設定は適宜行ってください
-      iOS: DarwinInitializationSettings(),
-    ),
-    onDidReceiveNotificationResponse: (details) {
-      if (details.payload != null) {
-        final payloadMap =
-        json.decode(details.payload!) as Map<String, dynamic>;
-        debugPrint(payloadMap.toString());
+    _initNotification();
+  }
+
+  Future<void> _initNotification() async {
+    final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      final notification = message.notification;
+      if (!kIsWeb && notification != null) {
+        await flutterLocalNotificationsPlugin.show(
+          0,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              'default_notification_channel',
+              'プッシュ通知のチャンネル名',
+              importance: Importance.max,
+              icon: notification.android?.smallIcon,
+            ),
+          ),
+          payload: json.encode(message.data),
+        );
       }
-    },
-  );
+    });
+
+    flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+        iOS: DarwinInitializationSettings(),
+      ),
+      onDidReceiveNotificationResponse: (details) {
+        if (details.payload != null) {
+          final payloadMap =
+              json.decode(details.payload!) as Map<String, dynamic>;
+          debugPrint(payloadMap.toString());
+        }
+      },
+    );
+  }
+
+  void _showNoConnectionDialog() {
+    if (_isDialogShowing) return; // ダイアログが既に表示されている場合は表示しない
+
+    _isDialogShowing = true;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('ネットワーク接続なし'),
+          content: const Text('インターネットに接続されていません。接続を確認してください。'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _isDialogShowing = false;
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return page.Page(title: 'LatinOne');
+  }
 }
--- lib/screen/inbox.dart
+++ lib/screen/inbox.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
 
-class InboxPage extends StatelessWidget {
-  const InboxPage({super.key});
+class InboxPage extends StatefulWidget {
+  final List<Map<String, dynamic>> notifications;
+
+  const InboxPage({super.key, required this.notifications});
+
+  @override
+  _InboxPageState createState() => _InboxPageState();
+}
+
+class _InboxPageState extends State<InboxPage> {
+  @override
+  void initState() {
+    super.initState();
+    print('通知リスト: ${widget.notifications}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inbox'),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(32.0),
-        child: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('To be continued...'),
-            ],
-          ),
-        ),
-      ),
+      body: widget.notifications.isEmpty
+          ? const Center(
+              child: Text('通知はありません'),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: widget.notifications.length,
+              itemBuilder: (context, index) {
+                final notification = widget.notifications[index];
+                final title = notification['title'] is String
+                    ? notification['title']
+                    : 'No Title';
+                final body = notification['body'] is String
+                    ? notification['body']
+                    : 'No Body';
+                final isRead = notification['isRead'] ?? false;
+
+                return Card(
+                  child: ListTile(
+                    title: Text(
+                      title,
+                      style: TextStyle(
+                        fontWeight:
+                            isRead ? FontWeight.normal : FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(body),
+                    trailing: isRead
+                        ? const Icon(Icons.check, color: Colors.green)
+                        : null,
+                    onTap: () {
+                      if (title.contains('店舗')) {
+                        Navigator.pop(context, 'shops');
+                      } else if (title.contains('商品')) {
+                        Navigator.pop(context, 'products');
+                      }
+                    },
+                  ),
+                );
+              },
+            ),
     );
   }
 }
